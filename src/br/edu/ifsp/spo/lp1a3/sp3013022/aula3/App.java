81eae709ca7aca9afbc47f09e3d9cb56658d7fe4
Luana Tavares Faiotto <luana.faiotto@gmail.com>
2019-03-26 11:09:28 -0300

    Criação do App.java

---
+++ src/br/edu/ifsp/spo/lp1a3/sp3013022/aula3/App.java
@@ -0,0 +1,40 @@
+package br.edu.ifsp.spo.lp1a3.sp3013022.aula3;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		String nomeTitular;
+		String numConta;
+		double saldoInicial;
+		double saldoChequeEspecial;
+//		int auxiliar;
+		
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.print("Digite o nome do titular: ");
+		nomeTitular = ler.nextLine();
+		
+		System.out.print("Digite o número da conta: ");
+		numConta = ler.nextLine();
+		
+		System.out.print("Digite o saldo inicial: ");
+		saldoInicial = ler.nextDouble();
+		
+		System.out.print("Insira o limite da cheque especial: ");
+		saldoChequeEspecial = ler.nextDouble();
+		
+		Conta cc = new ContaCorrente(numConta, nomeTitular, saldoInicial, saldoChequeEspecial);
+		
+//		System.out.println("Bem Vindo ao Banco XPTO!");
+//		System.out.println("Se você deseja se cadastrar, digite 1: ");
+		// Há um problema. Indicar qual é o problema, o por quê do problema e como corrigí-lo
+		// Não é um problema da Orientação, mas pode arrumar com isso com Orientação
+		// Se souber, mandar um email para ele
+		
+
+	}
+
+}
